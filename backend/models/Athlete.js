2522d39f8e97f193ed2088148d8ec22445a5dfd6
INDRESH VERMA <indreshkverma2004@gmail.com>
2025-05-28 21:35:19 +0530

    updated the athlete backend

--- backend/models/Athlete.js
+++ backend/models/Athlete.js
@@ -6,10 +6,6 @@ const athleteSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },
-  id:{
-    type: String,
-    required: true,
-  },
   playerid: {
     type: String,
     unique: true,
